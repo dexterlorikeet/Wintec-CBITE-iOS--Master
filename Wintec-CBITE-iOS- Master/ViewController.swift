1617927738e85b125a578be74c11eb1bb9a782f9
Alistair Tiang <nzmicro10492@icloud.com>
2018-11-05 08:22:46 +1300

    Initial Commit

---
+++ Wintec-CBITE-iOS- Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wintec-CBITE-iOS- Master
+//
+//  Created by Ally Tiang on 11/4/18.
+//  Copyright © 2018 Ally Tiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
