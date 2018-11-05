096533942e87a6ffdd3db76b334df5f11053c730
Alistair Tiang <nzmicro10492@icloud.com>
2018-11-05 17:16:40 +1300

    Login screen (draft)

--- Screens/ViewController.swift
+++ Screens/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func login(_ sender: Any) {
+        NSLog(" login");
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
