787a9ac395773b9d2b56018ad5f7d7a90f6f2005
Alistair Tiang <nzmicro10492@icloud.com>
2018-11-05 21:31:45 +1300

    added swipe feature, to the main menu

--- Screens/CourseViewController.swift
+++ Screens/CourseViewController.swift
@@ -11,14 +11,42 @@ import UIKit
 
 class CourseViewController: UIViewController {
     
+    
     @IBAction func login(_ sender: Any) {
         NSLog(" login");
     }
+    
+    
+    @IBOutlet weak var swipeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        
+        
     }
     
+    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
+        
+        if (sender.direction == .left) {
+            print("Swipe Left")
+            let labelPosition = CGPoint(x: self.swipeLabel.frame.origin.x - 50.0, y: self.swipeLabel.frame.origin.y)
+            swipeLabel.frame = CGRect(x: labelPosition.x, y: labelPosition.y, width: self.swipeLabel.frame.size.width, height: self.swipeLabel.frame.size.height)
+        }
+        
+        if (sender.direction == .right) {
+            print("Swipe Right")
+            let labelPosition = CGPoint(x: self.swipeLabel.frame.origin.x + 50.0, y: self.swipeLabel.frame.origin.y)
+            swipeLabel.frame = CGRect(x: labelPosition.x, y: labelPosition.y, width: self.swipeLabel.frame.size.width, height: self.swipeLabel.frame.size.height)
+        }
+    }
     
     
 }
--- Wintec-CBITE-iOS- Master/AppDelegate.swift
+++ Wintec-CBITE-iOS- Master/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        RunLoop.current.run(until: Date(timeIntervalSinceNow: 1.5));
+        RunLoop.current.run(until: Date(timeIntervalSinceNow: 2.0));
                 // Override point for customization after application launch.
         
         //let_ = Course()
