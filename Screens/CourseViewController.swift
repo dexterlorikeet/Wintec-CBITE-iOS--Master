f72da2d5b0d0f5988d4a7d6cd883d5c4cf20962b
Alistair Tiang <nzmicro10492@icloud.com>
2018-11-05 17:52:58 +1300

    Push view Controller to another view controller

---
+++ Screens/CourseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CourseViewController.swift
+//  Wintec-CBITE-iOS- Master
+//
+//  Created by Ally Tiang on 11/5/18.
+//  Copyright © 2018 Ally Tiang. All rights reserved.
+//
+
+
+import UIKit
+
+class CourseViewController: UIViewController {
+    
+    @IBAction func login(_ sender: Any) {
+        NSLog(" login");
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+}
--- Screens/ViewController.swift
+++ Screens/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
 
     @IBAction func login(_ sender: Any) {
         NSLog(" login");
+        
+        // Safe Present
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CourseViewController") as? CourseViewController
+        {
+            present(vc, animated: true, completion: nil)
+        }
+        
+        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
